c7671c1da937b92ba0f6433c07c066a601a4923a
ROHITH M <93328109+ROHITH-M10@users.noreply.github.com>
2024-08-04 00:22:16 +0530

    Add files via upload

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Data from './Data';
+import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+// Render the root component, which includes the Router and Routes setup
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Data />
+  </React.StrictMode>
+);
